2ffa0303dfdbed0c064caede910aff581e50e4df
hooq77 <huhooo@126.com>
2018-03-29 17:54:49 +0800

    关系建立，hasOne关系有问题

--- server/sequelize.js
+++ server/sequelize.js
@@ -35,7 +35,12 @@ var User = sequelize.define('user', {
   occupation: Sequelize.DataTypes.STRING(20),
   reg_time: Sequelize.DataTypes.DATE,
   last_login: Sequelize.DataTypes.DATE
+}, {
+  freezeTableName: true,
+  tableName: "sys_user"
 });
+
+
 
 var Building = sequelize.define('building', {
   id: {
@@ -55,8 +60,11 @@ var Building = sequelize.define('building', {
   center_point: Sequelize.DataTypes.GEOMETRY('Point', 4326),
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
+}, {
+  freezeTableName: true,
+  tableName: "indoor_building"
 });
-Building.belongsTo(User);
+
 
 var BuildingHistory = sequelize.define('buildingHistory', {
   key:{
@@ -83,6 +91,9 @@ var BuildingHistory = sequelize.define('buildingHistory', {
   center_point: Sequelize.DataTypes.GEOMETRY('Point', 4326),
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
+}, {
+  freezeTableName: true,
+  tableName: "history_building"
 });
 
 var Floor = sequelize.define('floor', {
@@ -100,10 +111,12 @@ var Floor = sequelize.define('floor', {
   },
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
+}, {
+  freezeTableName: true,
+  tableName: "indoor_floor"
 });
 
-Floor.belongsTo(Building);
-Building.belongsTo(User);
+
 var FloorHistory = sequelize.define('floorHistory', {
   key:{
     type:Sequelize.DataTypes.INTEGER,
@@ -127,7 +140,12 @@ var FloorHistory = sequelize.define('floorHistory', {
   },
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
+}, {
+  freezeTableName: true,
+  tableName: "history_floor"
 });
+
+
 var Area = sequelize.define('area', {
   id:{
     type: Sequelize.DataTypes.UUID,
@@ -143,7 +161,12 @@ var Area = sequelize.define('area', {
   },
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
-})
+}, {
+  freezeTableName: true,
+  tableName: "indoor_area"
+});
+
+
 var AreaHistory = sequelize.define('areaHistory', {
   key:{
     type:Sequelize.DataTypes.INTEGER,
@@ -166,7 +189,11 @@ var AreaHistory = sequelize.define('areaHistory', {
   },
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
+}, {
+  freezeTableName: true,
+  tableName: "history_area"
 })
+
 var Style = sequelize.define('style', {
   id:{
     type: Sequelize.DataTypes.INTEGER,
@@ -180,7 +207,11 @@ var Style = sequelize.define('style', {
   "stroke-opacity": Sequelize.DataTypes.REAL,
   fill: Sequelize.DataTypes.STRING(10),
   "fill-opacity": Sequelize.DataTypes.REAL,
+}, {
+  freezeTableName: true,
+  tableName: "sys_style"
 });
+
 
 var Poi = sequelize.define('poi', {
   id: {
@@ -200,7 +231,11 @@ var Poi = sequelize.define('poi', {
   },
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
+}, {
+  freezeTableName: true,
+  tableName: "indoor_poi"
 })
+
 
 var PoiHistory = sequelize.define('poiHistory', {
   key:{
@@ -227,7 +262,10 @@ var PoiHistory = sequelize.define('poiHistory', {
   },
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
-})
+}, {
+  freezeTableName: true,
+  tableName: "history_poi"
+});
 
 var Line = sequelize.define('line', {
   id: {
@@ -243,7 +281,10 @@ var Line = sequelize.define('line', {
   one_way: Sequelize.DataTypes.BOOLEAN,
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
-})
+}, {
+  freezeTableName: true,
+  tableName: "indoor_line"
+});
 
 var LineHistory = sequelize.define('lineHistory', {
   key:{
@@ -266,8 +307,45 @@ var LineHistory = sequelize.define('lineHistory', {
   one_way: Sequelize.DataTypes.BOOLEAN,
   time: Sequelize.DataTypes.DATE,
   version: Sequelize.DataTypes.INTEGER
-})
-sequelize.sync()
+}, {
+  freezeTableName: true,
+  tableName: "history_line"
+});
+
+User.hasMany(Building, {foreignKey: "owner"});
+Building.belongsTo(User, {foreignKey: "owner"});
+
+User.hasMany(Floor, {foreignKey: "owner"});
+Floor.belongsTo(User, {foreignKey: "owner"});
+
+User.hasMany(Area, {foreignKey: "owner"});
+Area.belongsTo(User, {foreignKey: "owner"});
+
+User.hasMany(Poi, {foreignKey: "owner"});
+Poi.belongsTo(User, {foreignKey: "owner"});
+
+User.hasMany(Line, {foreignKey: "owner"});
+Line.belongsTo(User, {foreignKey: "owner"});
+
+Building.hasMany(Floor);
+Floor.belongsTo(Building);
+
+Floor.hasMany(Area);
+Area.belongsTo(Floor);
+
+Floor.hasOne(Style);
+Area.hasOne(Style);
+Area.hasOne(Poi);
+
+Poi.belongsToMany(Floor, {through: 'poi_floor'});
+Floor.belongsToMany(Poi, {through: 'poi_floor'});
+
+
+Line.hasOne(Poi, {as: "start", foreignKey: "from"});
+Line.hasOne(Poi, {as: "end", foreignKey: "to"});
+
+
+sequelize.sync({force: true})
 
 //
 // const User = sequelize.define('user', {
